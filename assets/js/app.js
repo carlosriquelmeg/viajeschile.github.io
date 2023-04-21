187d6e6eded78b539551f584ebfd65725161e7c9
Carlos <carlosriquelmegodoy@gmail.com>
2023-04-21 18:34:29 -0400

    Se agregan enlances anclas y mouseenter y leave a textos de cards quienes somos

---
+++ assets/js/app.js
@@ -0,0 +1,51 @@
+
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+  });
+
+$(document).ready(function(){
+    $("#enviarCorreo").click(function(){
+      alert("El correo fue enviado correctamente");
+    });
+  });
+
+
+  $(document).ready(function(){
+    $("#texto-card1").mouseenter (function(){
+      $(this).css("color", "#00CEF5");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#texto-card1").mouseleave (function(){
+      $(this).css("color", "white");
+    });
+  });
+  $(document).ready(function(){
+    $("#texto-card2").mouseenter (function(){
+      $(this).css("color", "#00CEF5");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#texto-card2").mouseleave (function(){
+      $(this).css("color", "white");
+    });
+  });
+
+
+  $(document).ready(function(){
+    $("#texto-card3").mouseenter (function(){
+      $(this).css("color", "#00CEF5");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#texto-card3").mouseleave (function(){
+      $(this).css("color", "white");
+    });
+  });
+
+
+  
